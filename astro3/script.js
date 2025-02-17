2d660cdee8e30a02bc2197b886c2b37befb0359d
SHIVAMY007 <166384824+SHIVAMY007@users.noreply.github.com>
2025-02-17 16:49:50 +0530

    Add files via upload

---
+++ astro3/script.js
@@ -0,0 +1,13 @@
+
+const container = document.getElementById('box2-container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
